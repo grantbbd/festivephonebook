527ebcb53c176cf08cc8ac18eb180d17d81277f4
grant.vice <grantv@bbd.co.za>
2020-01-09 10:19:13 +0200

    template app

---
+++ Festive_Phonebook_App/Festive_Phonebook_App/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Festive_Phonebook_App.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
